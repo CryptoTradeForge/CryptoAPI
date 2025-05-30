fc76edc0e490068601d5fb75dc0a8af21b6f6baf
游宸睿 <asd47964773@gmail.com>
2025-05-30 15:46:52 +0800

    modify cmc_api function

--- cmc_api.py
+++ cmc_api.py
@@ -13,17 +13,37 @@ class CoinMarketCapAPI:
         if not self.CMC_API_KEY:
             raise ValueError("CoinMarketCap API key not found in environment variables.")
 
-    def get_top_cryptos(self, limit=None):
+        self.stable_coins = [
+            "USDT", "USDC", "USDe", "PYUSDT", "BUSD",
+        ]
+        self.problematic_coins = [
+            "HYPE",
+        ]
+
+    def get_top_cryptos(self, limit=None, exclude_stablecoins=True, filling=True):
         """Fetch top cryptocurrencies from CoinMarketCap."""
         url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
         headers = {"X-CMC_PRO_API_KEY": self.CMC_API_KEY}
         response = requests.get(url, headers=headers)
         data = response.json()["data"]
         
-        data = data[:limit] if limit and limit < len(data) else data
+        # filter out problematic coins
+        data = [coin for coin in data if coin["symbol"] not in self.problematic_coins]
+        
+        if not filling:
+            data = data[:limit] if limit and limit < len(data) else data
         top_cryptos = sorted(data, key=lambda x: x["cmc_rank"])
         
-        top_cryptos_list = [f"{i['symbol']}USDT" for i in top_cryptos]
+        if exclude_stablecoins:
+            top_cryptos_list = [f"{i['symbol']}USDT" for i in top_cryptos if i['symbol'] not in self.stable_coins]
+        else:
+            top_cryptos_list = [f"{i['symbol']}USDT" for i in top_cryptos]
+        
+        if filling and limit is not None:
+            top_cryptos_list = top_cryptos_list[:limit]
+        
+        if not top_cryptos_list:
+            raise ValueError("No valid cryptocurrencies found in the top list.")
         
         return top_cryptos_list
     
